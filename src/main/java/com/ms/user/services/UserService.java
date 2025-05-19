dc4aaee90c7a9dd54b5c3607f9606aba72c201d9
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2025-05-19 01:22:42 -0300

    mensageria rabbitmq

--- src/main/java/com/ms/user/services/UserService.java
+++ src/main/java/com/ms/user/services/UserService.java
@@ -3,6 +3,7 @@ package com.ms.user.services;
 import org.springframework.stereotype.Service;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -11,15 +12,18 @@ import jakarta.transaction.Transactional;
 public class UserService {
 	
 	final UserRepository userRepository;
+	final UserProducer userProducer;
 	
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, UserProducer userProducer) {
 		this.userRepository = userRepository;
+		this.userProducer = userProducer;
 	}
 	
 	@Transactional
 	public UserModel save(UserModel userModel) {
-		return userRepository.save(userModel);
-		
+		userModel = userRepository.save(userModel);
+		userProducer.publishMessageEmail(userModel);
+		return userModel;
 	}
 
 }
